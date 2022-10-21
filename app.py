b189df9862758420e8f8de8b942ed760b8d5d79c
정훈 <greenjm20214@gmail.com>
2022-10-21 06:48:20 +0000

    Server First Code commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import asyncio
+import websockets
+async def accept(websocket, path): 
+    while True:
+        data = await websocket.recv()
+        print("receive : " + data)
+        await websocket.send("echo : " + data)
+
+start_server = websockets.serve(accept, "0.0.0.0", 5050)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
