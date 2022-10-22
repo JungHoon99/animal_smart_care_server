53ca268adb7abad94bb8f8d9ed6678465d537b0a
정훈 <greenjm1@nate.com>
2022-10-22 12:02:11 +0000

    Room InterFace Append

--- app.py
+++ app.py
@@ -5,19 +5,22 @@ from ast import literal_eval
 Room = {}
 
 async def device(websocket,data):
-    Room[data['roomNumber']]
+    Room[data['roomNumber']]={'device':websocket,'client':[]}
+    print(Room)
 
-async def User(websocket):
-    pass
+async def User(websocket,data):
+    Room[data['roomNumber']]['client'].append(websocket)
+    print(Room)
+    
+
 async def Main(websocket, path):
     ClientList.add(websocket)
     data = await websocket.recv()
     data = literal_eval(data)
-    if(data['tick'])
-    
-    
-    
-    
+    if(data['kind'] == 0):
+        await device(websocket,data)
+    else:
+        await User(websocket,data)
 
 start_server = websockets.serve(Main, "0.0.0.0", 5050)
 asyncio.get_event_loop().run_until_complete(start_server)
