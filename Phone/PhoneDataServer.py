7722b7d97dfe390ddcae182231cf8d857f47ebaa
정훈 <greenjm1@nate.com>
2022-11-15 14:06:16 +0000

    Phone Data Server Insert success

--- Phone/PhoneDataServer.py
+++ Phone/PhoneDataServer.py
@@ -2,19 +2,25 @@ from MySqlConnect import MufiData
 import asyncio
 import websockets
 from ast import literal_eval
+import json
 
 async def Main(websocket,path):
     data = await websocket.recv()
     data = literal_eval(data)
     md  = MufiData()
     sendMessage = {"Title":"","message":""}
+    
     if(data["kind"]=="select"):
         data = md.selectdb(data["message"])
         sendMessage["Title"] = "Select success"
         sendMessage["message"] = data
-        await websocket.send(str(sendMessage))
+        sendStr = json.dumps(sendMessage)
+        await websocket.send(sendStr)
     elif(data["kind"]=="insert"):
-        pass
+        data = md.insertdb(data["message"])
+        sendMessage["message"] = data
+        sendStr = json.dumps(sendMessage)
+        await websocket.send(sendStr)
     else:
         sendMessage["Title"] = "Try Again"
         await websocket.send(str(sendMessage))
